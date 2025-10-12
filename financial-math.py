8b61bde744b4df973a47574f8b29e0afad188e4b
Jonny255 <jonathan.m1078@gmail.com>
2025-10-11 22:51:22 -0500

    chromebook

--- financial-math.py
+++ financial-math.py
@@ -1,56 +1,326 @@
 import os
-import pylance
 import math
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
-
-
-
-
+"""
+what's the difference between building a financial instrument vs investment vehicle.
+        I think investment vehicle ismore macro, like stocks, 
             TODO
-    [ ] build calculator for compound interest, compounded daily, but APY is also updated
+    [x] build calculator for compound interest, compounded daily, but APY is also updated
         daily
     [ ] 
 
-
-# compound interest variables
-int A = [final amount]
-int P = [initial principle balance]
-int r = [interest rate]
-int t = [number of time periods elapsed] # the amount of time (usually in yrs) that the money is saved, invested, or borrowed
-int n = [] # Amount of times the interest is compo8unded per time period (t)
+"""
 
 
+class main:
+        def compounding_frequency(self):
+                self.daily_compounding = 365
+                self.monthly_compounding = 12
+                self.annually_compounding = 1
 
-# latest values of coins staked/to be staked.
-list[T]: [AXS, COMP, AVS, CRO, CRV, DAI]
-coin = "coin"
-APY = 00.00 # as of right now/today
+                print("Please select 'daily', 'monthly', or 'annually'.")
+                compounding_frequency = input()
 
-# actual rates percentage values
-rate_0 = APY
-rate_30 = ""
-rate_90 = ""
-rate_180 = ""
-
-rate_change_30 = sum(rate_30 - rate_0)
-rate_change_90 = sum(rate_90 - rate_30)
-rate_change_180 = sum(rate_180 - rate_90)
-
-tolerance_30 = sum(rate_change_30 / APY)
-tolerance_90 = sum(rate_change_90 / APY)
-tolerance_180 = sum(rate_change_180 / APY)
-
-
-
-
-
-
-# compound formula
-A = P(1 + r / n)^nt
-
-
+                if input == "daily":
+                        compounding_frequency = self.daily_compounding
+                elif input == "monthly":
+                        compounding_frequency = self.monthly_compounding
+                elif input == "annually":
+                        compounding_frequency = self.annually_compounding
+                else:
+                        raise Exception("ERROR: Please select 'daily', 'monthly', or 'annually'.")
+            
+            
+        def APY_calculator(self):
+                
+                # compound interest variables
+                self.A = 0 # final amount
+                self.P = 0 # initial principle balance
+                self.r = 42 # interest rate
+                self.t = 12 # number of time periods elapsed the amount of; time (usually in yrs) that the money is saved, invested, or borrowed
+                self.n = self.daily_compounding # Amount of times the interest is compounded per time period (t)
 
 
+                # PEMDAS = "Please Excuse My Dear Aunt Sally"
+
+                # A = P * (1 + r / n) * pow(365, 12)
+                self.nt = (self.n * self.t)
+                self.A = self.P * math.pow((1 + self.r / self.n), self.nt)
+            
+        def intrate_calculator(self):
+        #        r = n[(A/P)^(1/nt) - 1]
+                r = (self.n) * [math.pow((self.A / self.P), self.nt)]
 
 
+        # latest values of coins staked/to be staked.
+
+
+        coin = ["AXS", "COMP", "AVS", "CRO", "CRV", "DAI"]
+
+        for x in coin:
+                print(coin)
+
+
+        # Calculate the date 30 days before today's date
+        dt_0 = date.today() - timedelta(30)
+
+        # Print the current date using date.today()
+        print('Current Date :', date.today())
+
+        # Print the date calculated 5 days before the current date
+        print('5 days before Current Date :', dt_0) 
+
+
+        dt_30 = dt_0 - relativedelta(months=1)
+        dt_90 = dt_0 - relativedelta(months=3)
+        dt_180 = dt_0 - relativedelta(months=6)
+
+
+
+            
+
+                # actual rates percentage values
+        def rate(self):
+                APY = 42 # as of right now/today
+                rate_0 = APY
+                rate_30 = 46
+                rate_90 = 48
+                rate_180 = 51
+
+        def rate_change(self):
+                rate_change_30 = "rate_30 - rate_0"
+                rate_change_90 = "rate_90 - rate_30"
+                rate_change_180 = "rate_180 - rate_90"
+
+        def tolerance(self):
+                tolerance_30 = "rate_change_30 / APY"
+                tolerance_90 = "rate_change_90 / APY"
+                tolerance_180 = "rate_change_180 / APY"
+
+
+                # Stock Percentage Calculator
+        def stock_percentage_calculator(self):
+                old_price = 0
+                new_price = 0
+
+                # Formula      
+                # RoR = (latest price - initial price) / latest price
+                
+                RoR = (new_price - old_price) / new_price
+
+                if old_price < new_price:
+                        perc = RoR + 1
+                        print(perc)
+                elif old_price > new_price:
+                        perc = RoR
+                        print(perc)      # -0.032353984
+                elif old_price == new_price:
+                        RoR = perc
+
+
+                a = old_price - new_price
+
+                if new_price < old_price:
+                        perc = a / old_price
+                elif new_price > old_price:
+                        perc = a * 3
+                        
+
+
+
+            # compound formula
+            # A = P(1 + r / n)^(n * t)
+
+
+                staking_pnt_USDC = 4.1 #%
+                staking_pnt_ETH = 4.1 #%
+
+            # nt = math.pow(n, t)
+
+
+        def value_savings(self):
+        #        cost_of_subscription_plan
+                subscription_plan_cost = ""
+        #       Basic Benefits
+                CoinbaseOne_Basic_monthly = 4.99
+                CoinbaseOne_Basic_annual = 49.99
+                CoinbaseOne_Preferred_monthly = 49.99
+                CoinbaseOne_Preferred_annual = 499.88
+                CoinbaseOne_Premium_monthly = 299.99
+                CoinbaseOne_Premium_annual = 2999.99
+                
+                print("Select Plan Below:")
+                
+                print("1a. Coinbase One Basic Monthly")
+                print("1b. Coinbase One Basic Annual")
+                 
+                print("2a. Coinbase One Preferred Monthly")
+                print("2b. Coinbase One Preferred Annual")
+                
+                print("3a. Coinbase One Premium Monthly")
+                print("3b. Coinbase One Premium Annual")
+
+
+
+                if input() == "1a" or "Coinbase One Basic monthly":
+                        benefits_package = CoinbaseOne_Basic_monthly
+                elif input() == "1b" or "Coinbase One_Basic_annual":
+                        benefits_package = CoinbaseOne_Basic_annual
+                elif input() == "2a" or "CoinbaseOne_Preferred_monthly":
+                        benefits_package = CoinbaseOne_Preferred_monthly
+                elif input() == "2b" or "CoinbaseOne_Preferred_annual":
+                        benefits_package = CoinbaseOne_Preferred_annual
+                elif input() == "3a" or "CoinbaseOne_Premium_monthly":
+                        benefits_package = CoinbaseOne_Premium_monthly
+                elif input() == "3b" or "CoinbaseOne_Premium_monthly":
+                        benefits_package = CoinbaseOne_Premium_annual
+
+                def trading_fees(self):
+                        trading_fees = ""
+                        trading_fee_tracker = 0
+                        if trading_fee_tracker <= 500:
+                                trading_fees = 0
+                        else:
+                                trading_fees = dnr
+                        
+
+
+                def CoinbaseOne_Basic(self):
+                        trading_fees = trading_fees
+                        earn_btc_back = 1.04 # Coming soon Coinbase One Card.
+                        Account_protection = 1000
+                        earn_usdc_rewards = 4.5 # Boosted rewards on your first 10k USDC
+
+                        #### NOTE #### The 5% boost is of the APY, not in addition to the APY
+                        earn_more_staking = 5 #% on eligible staking assets
+
+                        member_sweepstakes = "like winning 1 BTC"
+                        onchain_benefits = "With Base, Aerodrome, and more"
+
+                def CoinbaseOne_Preferred(self):
+                        trading_fees = trading_fees # $500/mo in trades, a spread applies
+                        earn_btc_back = 1.04 # Coming soon Coinbase One Card.
+                        Account_protection = 10000
+                        earn_usdc_rewards = 0.045 # Boosted rewards on your first 30k USDC
+                        earn_more_staking = 5
+                        member_sweepstakes = "like winning 1 BTC"
+                        onchain_benefits = "With Base, Aerodrome, and more"
+
+                def CoinbaseOne_Premium(self):
+                        trading_fees = trading_fees # $500/mo in trades, a spread applies
+                        earn_btc_back = 4 # Coming soon Coinbase One Card.
+                        Account_protection = 250000
+                        earn_usdc_rewards = 1.045 # Boosted rewards; unlimited
+                        earn_more_staking = 5
+                        member_sweepstakes = "like winning 1 BTC"
+                        onchain_benefits = "With Base, Aerodrome, and more" 
+
+
+
+        
+
+
+
+
+
+
+        def number_crunching(self):
+
+        coinbaseOne basic monthly = 4.99
+        coinbaseOne basic annual = 49.99
+        potential_savings_prepay = 17%
+
+        stock market index = 12% APY (Daily compounding included)
+        annual inflation = 2.9%
+        credit card interest = 14 - 32%
+        credit card reward points = 
+
+        personal loan interest (prosper, upstart, lendingclub) = 7 - 28%
+
+        0% APR balance transfer promotion = 18 months
+        0% APR purchase promotion = 18 months
+
+        borrowing against crypto assets = 18% ?
+        lending 
+
+
+
+        try
+                sort 
+
+
+
+        Synthetic CDO vs 
+
+
+        def fractional_reserve_banking():
+                # The Federal Reserve
+                Initial_bal = 1000
+                Reserved_pct = 10
+                Amt_to_lend_pct = 90
+                lent_amt_1 = 900
+                amt_reserved = 100
+                lent_amt_1_apy = 0.0191
+
+                bal_2 = 900
+                lent_amt_2 = 810
+                amt_reserved = 90
+
+                bal_3 = 729
+
+        def CDO():
+                jameithealtcoin =  [
+                        BTC  : 25%
+                        ETH  : 20%
+                        USDT : 50%
+                        AAVE : 5%
+                ]
+
+
+        def CDO_synthetic(self):
+                asset_to_stake = CDO
+                self.convert(asset_to_stake)
+                # Use this CDO as a currency, and then stake it on eth; or could I create my own coin
+                # that IS the CDO, have it ETH based, and then could I stake that for stETH and then
+                # borrow USDC against that stETH?
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+S"""
+
+
+
+
+
+
+try:
+  print(x)
+except:
+  print("An exception occurred")
+
+
+
+
+
+
+
+
+"""
---
+++ financial-strategies.md
@@ -0,0 +1,88 @@
+# Financial Strategies
+
+## Loan Replayment
+
+### Simple, Basic Strategies
+
+#### Strategy #1: Pay more than the minimum monthly payments
+
+Paying more than the minimum monthly debt payments chips away a larger chunk of the principal portion of your debt, so you can save money on interest and speed up your debt payoff. It can work for all types of debt, from student loans, medical bills and personal loans to auto loans and credit card debt.
+
+Even a small increase in your monthly payment can have a big impact. Start with what fits your budget—whether that’s $20 or $200 extra per month. The key is being consistent with those extra payments.
+
+#### Strategy #2: Avalanche Method
+
+1. Make a list of all your loans sorted by highest-interest at the top, and lowest-interest at the bottom
+2. Pay the minimum on all of the loans.
+3. If/when you have extra money put it towards the hightest-interest at the top. Continue with minimum payments on the rest of the loans.
+4. Once that top debt is paid, use all of that extra money (the principle + interest you were paying, plus the extra money you'd add on top) to pay off the next highest-interest loan, which has now taken the top one on the list.
+5. Continue repeating this until all debts are repayed.
+
+#### Strategy #3: Snowball Method
+
+1. List all debts sorted from smallest balance at the top of the list, and the largest balance at the bottom of the list.
+2. Make minimum payments on all of the loans.
+3. If/when you have extra money put it towards paying off the smallest balance loan at the top. Continue with minimum payments on the rest of the loans.
+4. Once that top debt is paid, use all of that extra money (the principle + interest you were paying, plus the extra money you'd add on top) to pay off the next smallest-balance loan, which has now taken the top one on the list.
+5. Continue repeating this until all debts are repayed.
+
+#### Strategy 4: Transfer Balance to a Lower-Interest Rate Card/Loan
+
+1. Pretty self explanatory.
+
+#### Strategy 5: Debt Consolidation Loan
+
+1. Combining all of your debts, especially if you have a lot of high-interest loans, into one big loan but with a smaller interest rate can help paying off the debt with a single loan instead of multiple, and at the lower interest rate.
+2. Get a(nother) loan (unsecured uncollateralized loan is fine).
+3. Ensure that the loan is equal or less than your current existing debt's interest rates. Loan should also be equal to or slightly greater than the total balance of all your debts combined.
+4. Use this loan to pay off all of your previous debts, and now you only have one single loan to worry about paying off, and it'll also hopefully be at a lower interest rate.
+
+### Advanced Strategies
+
+#### Strategy 6: Convert Bad Debt into Good Debt
+
+#### Strategy 7: Using negative interest to curb inflation/leverage inflation to help pay off your debt
+
+Meant to only work well with and between commercial banks and the central bank (The Fed). It becomes significantly more complex and therefore riskier, however negative interest rates could help retail borrowers with receiving interest payments on loans, such as mortgages or personal loans, effectively reducing the principal amount owed over time.
+
+In practice, this policy is designed to discourage saving and encourage borrowing and consumption. When banks are charged for keeping reserves, they may pass on the cost by reducing the interest paid on savings accounts or even charging fees to depositors. However, commercial banks are often reluctant to pass negative rates directly onto consumers, potentially leading to increased fees or reduced lending instead. For borrowers, negative interest rates mean they could receive interest payments on loans, such as mortgages or personal loans, effectively reducing the principal amount owed over time.
+
+#### Strategy 8: The thing with stocks where you leverage them to pay off debt while your investment(s) continue compounding
+
+## Budgeting Strategies
+
+### Simple, Basic Budgeting Strategies
+
+#### Strategy 1: Bucket/Envelope Method
+
+1. Every kind of expenditure you have would have money allocated for it in an envelope or "bucket".
+2. For example, you have a money income of $5,000. Grab an envelope and label it "Food", and a certain dollar-figure amount like $500 ($125/wk). So of the $5,000 you made, place $500 of that into this envelop. And that is the monthly budget you have alloted for yourself to spend on food in a month.
+3. Next, grab another folder and label it "Discretionary Spending" with a budget amount of say $250/mo. Just like before, place $250 from your remaining $4,500 into this envelope and set it aside. You now have $250 you could spend each month on whatever "stuff" you want to spend money on, like Netflix, save up for an annual vacation to another country, etc.
+4. Continue repeating the steps above working your way down from your remaining $4,250 until everything is allocated towards a category and in an envelope.
+
+#### Strategy 2: Zero-Budget Method
+
+The idea is to somewhat be frugal with your expenses/spending, and to end the month with having a balance of $0 in your bank account. Like the envelop method, you'd want to account for every dollar and have it allocated towards something. You don't want to get in the negative for obvious reasons, and you also don't want you have any extra money "left over" because that doesn't help you in any way. So if you do have money left over at the end of the month, these are the 3 most common categories where people put their remaining funds into.
+
+ 1. Use this leftover money to pay off your debts sooner.
+ 2. Add this money into your rainy-day fund (remember, ideal rainy-day budget is for at least 6 months to 1 year with your current lifestyle.)
+ 3. Add this money to your investment portfolio.
+ 4. Add this money to your discretionary spending pool.
+
+### Advanced Budgeting Strategies
+
+#### Copy the Federal Annual Budget Process
+
+This is where congress works on predicting its revenue for the following fiscal year, and all of the expenses it will most likely have. So if for example your annual salary is $75,000, you wouldn't be looking at your mortgage/rent as $1,200/mo, but instead it will cost you 14,400 a year. Your $280 auto insurance premium each month will be $3,360 for the year. That's easy for all of your fixed expenses for the year and allocate funds for. For variable expenses such as food and fuel, you want to add up all of the receipts you collected the past year from your grocery store and gas station, add up those totals, and now you could use that as a baseline amount you need to allocate for each. You could also multiply that amount by the current inflation rate (ie 3%, or 1.03) along with monthly trends if you want to forecast even more accurate numbers for your budget.
+
+#### Compare monthly pay to prepay (3-months, 6-months, 12-months, etc)
+
+Compare and contrast the savings you'll realize if you prepay _x_ amount of months ahead versus continuing to pay monthly. Works very well with complementing the federal budget process.
+
+## Investment Strategies
+
+### Simple, Basic Investment Strategies
+
+### Advanced Investment Strategies
+
+For prepayment of goods and services options, crunch numbers to determine which prepayment plan/option will yield you the most return on your investment. Then take that percentage
